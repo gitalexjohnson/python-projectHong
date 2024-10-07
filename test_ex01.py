7cd975e6ac10f672a7e67fcff33d107db2e5e1d7
Hong <52814111+hongfu2019@users.noreply.github.com>
2024-10-07 15:09:47 +0800

    Update test_ex01.py

--- test_ex01.py
+++ test_ex01.py
@@ -7,7 +7,7 @@ def add(a, b):
 """
 class TestAddFunction(unittest.TestCase):
     def test_add_positive(self):
-        self.assertEqual(add(1, 2), 4)
+        self.assertEqual(add(1, 2), 3)
 
     def test_add_negative(self):
         self.assertEqual(add(-1, -1), -2)
